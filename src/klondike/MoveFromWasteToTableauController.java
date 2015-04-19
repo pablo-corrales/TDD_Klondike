8f5b71e685f35d8d5d97d8d44c48f7d2cb9104c0
pablo-corrales <charliequiro>
2015-04-19 23:56:27 +0200

    Se implementan más métodos para probar movimientos de Waste a Tablue. Pasan los test. Se refactoriza código

--- src/klondike/MoveFromWasteToTableauController.java
+++ src/klondike/MoveFromWasteToTableauController.java
@@ -4,39 +4,57 @@ import java.util.Stack;
 
 public class MoveFromWasteToTableauController {
 	private StartGameController startGameController;
+	private Card wasteCard;
 
 	public MoveFromWasteToTableauController(StartGameController startGameController) {
 		this.startGameController = startGameController;
+		
 	}
 
 	public boolean moveFromWasteToTableau(int tableau) {
-		Stack<Card> tableauCard = startGameController.getTableauCard(tableau);
- 		Card wasteCard = startGameController.oneCardFromWaste();
- 		
+		this.wasteCard = startGameController.oneCardFromWaste();
+		Stack<Card> tableauCard = this.startGameController.getTableauCard(tableau);		
  		if( (tableauCard.size() == 0) && (wasteCard.getCard() == 12)){
- 					return moveToTableau(tableau, wasteCard);
+ 					return moveToTableau(tableau);
  		}
  		else if(tableauCard.size() > 0)
- 					return tableauCardSizeGreatThanZero(tableau, tableauCard, wasteCard);		
+ 					return tableauCardSizeGreatThanZero(tableau, tableauCard);		
  			 			
 		return false;
 	}
 	
 		
-	public boolean tableauCardSizeGreatThanZero(int tableau, Stack<Card> tableauCard, Card wasteCard){
+	public boolean tableauCardSizeGreatThanZero(int tableau, Stack<Card> tableauCard){
 			Card lastCardTableau = tableauCard.peek();
-			if((lastCardTableau.getCard()==(wasteCard.getCard()+1))
-					&&(lastCardTableau.getColorCard()!=wasteCard.getColorCard())){
-				return moveToTableau(tableau, wasteCard);
+			if(sameLastCardTableuAndWaste(lastCardTableau)
+					&&(sameColorLastCardWasteAndTableau(lastCardTableau))){
+				return moveToTableau(tableau);
 			}
 			
 			return false;
 	}
 	
 	
-	public boolean moveToTableau(int tableau, Card card){
-			startGameController.addCardToTableau(tableau, card);
-			startGameController.removeCardFromWaste();
+	public boolean sameLastCardTableuAndWaste(Card lastCardTableau){
+		
+		if ( lastCardTableau.getCard() == (wasteCard.getCard()+1) )
+			return true;
+		
+		return false;				
+	}
+	
+	
+	
+	public boolean sameColorLastCardWasteAndTableau(Card lastCardTableau){
+		if (lastCardTableau.getColorCard() != wasteCard.getColorCard())
+			return true;
+		return false;
+	}
+	
+	
+	public boolean moveToTableau(int tableau){
+			this.startGameController.addCardToTableau(tableau, wasteCard);
+			this.startGameController.removeCardFromWaste();
 			return true;
 	}
 	
--- src/klondike/MoveFromWasteToTableauControllerTest.java
+++ src/klondike/MoveFromWasteToTableauControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 
 public class MoveFromWasteToTableauControllerTest {
 	private static final int TABLEAU = 1;
-	private static final int FOUNDATION = 1;
 	private MoveFromWasteToTableauController moveFromWasteToTableauController;
  	private StartGameController startGameController;
  	
@@ -22,22 +21,44 @@ public class MoveFromWasteToTableauControllerTest {
  		
  	}
  	
+ 
  	@Test
  	public void moveFromWasteToTableauTest(){
- 			startGameController.addCardToWaste(new Card(2, CardSuit.SPADES));
-						
-			assertTrue(moveFromWasteToTableauController.moveFromWasteToTableau(TABLEAU));
-			
-			// En tableua está una carta 
-			assertEquals(1, startGameController.getTableauCard(TABLEAU).size());
-			
-			// Waste queda vacío
+ 		
+ 			startGameController.addCardToWaste(new Card(12, CardSuit.SPADES));
+ 									
+			assertTrue(moveFromWasteToTableauController.moveFromWasteToTableau(TABLEAU));			
+			assertEquals(1, startGameController.getTableauCard(TABLEAU).size());	
 			assertEquals(0, startGameController.sizeWaste());
-
- 			
- 			
+				
  	}
  	
+ 	@Test
+ 	public void allowedMoveFromWasteToTableauTest(){
+ 		
+			startGameController.addCardToTableau(TABLEAU, new Card(12, CardSuit.HEARTS));
+			startGameController.addCardToWaste(new Card(11, CardSuit.SPADES));	
+			
+			assertTrue(moveFromWasteToTableauController.moveFromWasteToTableau(TABLEAU));
+			
+			assertEquals(2, startGameController.getTableauCard(TABLEAU).size());	
+			assertEquals(0, startGameController.sizeWaste());
+				
+ 	}
+ 	
+ 	@Test
+ 	public void notAllowedMoveFromWasteToTableauTest(){
+ 		
+ 		
+ 			startGameController.addCardToWaste(new Card(4, CardSuit.SPADES));
+						
+			assertFalse(moveFromWasteToTableauController.moveFromWasteToTableau(TABLEAU));			
+			assertEquals(0, startGameController.getTableauCard(TABLEAU).size());	
+			assertEquals(1, startGameController.sizeWaste());
+		
+ 	}
+ 
  	
 
+ 	 	 	
 }
